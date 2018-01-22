50f82da8cdee13944359cea545716eef359d961f
Bor Hodošček <bor.hodoscek@gmail.com>
2018-01-22 15:55:28 +0900

    added supporting utils

---
+++ src/pdf_ja_paper_text_extractor/jp.clj
@@ -0,0 +1,39 @@
+(ns pdf-ja-paper-text-extractor.jp)
+
+(defn char-writing-system
+  "Return writing system type of char.
+  To get codepoint of char: `(.codePointAt char 0)`.
+  Converting code point into char: `(char 0x3041)`."
+  [^String ch]
+  (let [code-point (.codePointAt ch 0)]
+    (cond
+      (and (>= code-point 0x3041) (<= code-point 0x309f)) :hiragana
+      (and (>= code-point 0x4e00) (<= code-point 0x9fff)) :kanji
+      (and (>= code-point 0x30a0) (<= code-point 0x30ff)) :katakana
+      (or (and (>= code-point 65)    (<= code-point 122))    ; half-width alphabet (A-Za-z)
+          (and (>= code-point 65313) (<= code-point 65370))  ; full-width alphabet (Ａ-Ｚａ-ｚ)
+          (and (>= code-point 48)    (<= code-point 57))     ; half-width numbers  (0-9)
+          (and (>= code-point 65296) (<= code-point 65305))) ; full-width numbers  (０-９)
+      :romaji
+      (or (= code-point 12289) (= code-point 65291) (= code-point 44)) :commas ; [、，,] <- CHECK
+      :else :symbols)))
+
+(defn writing-system-count
+  "Returns a hash-map of character frequencies by writing system."
+  [s]
+  (merge {:hiragana 0
+          :kanji    0
+          :katakana 0
+          :romaji   0
+          :symbols  0
+          :commas   0}
+         (frequencies (map char-writing-system (map str s)))))
+
+(defn is-japanese? [filename s]
+  (let [ws-freqs (writing-system-count s)
+        length (count s)
+        japanese-ratio (if (pos? length)
+                         (/ (reduce + (vals (select-keys ws-freqs [:hiragana :katakana :kanji :commas]))) length)
+                         0.0)]
+    (if (> japanese-ratio 0.5)
+      true)))
---
+++ src/pdf_ja_paper_text_extractor/text.clj
@@ -0,0 +1,77 @@
+(ns pdf-ja-paper-text-extractor.text
+  (:require [clojure.string :as string]
+            [me.raynes.conch :as sh]
+            [pdfboxing.text :as text]
+            [pdf-ja-paper-text-extractor.jp :refer [is-japanese? char-writing-system]]
+            [corpus-utils.text :as utils]))
+
+(defn remove-title [title journal s]
+  (if-let [match (re-seq (re-pattern (format "(%s|%s)" title journal)) s)]
+    (let [maybe-sentence (-> s
+                             (string/replace title "")
+                             (string/replace (re-pattern (format "(?i)%s[\\s\\dvolumen\\.]*" journal)) ""))]
+      (if (and (not-empty maybe-sentence) (is-japanese? maybe-sentence ""))
+        maybe-sentence
+        (println (format "Rejecting: title='%s', journal='%s', s='%s', ms='%s'" title journal s maybe-sentence))))
+    s))
+
+(defn remove-space-between-chars [s]
+  "Removes spaces between Japanese character classes."
+  (if (> (count s) 2)
+    (str (subs s 0 2)
+         (string/join (filter identity
+                              (map (fn [b-c c a-c]
+                                     (let [b-c-s (char-writing-system (str b-c))
+                                           a-c-s (char-writing-system (str a-c))]
+                                       (cond
+                                         (and (#{:hiragana :katakana :kanji} b-c-s)
+                                              (re-seq #"[\s　]" (str c))
+                                              (#{:hiragana :katakana :kanji} a-c-s)) nil
+                                         (and (#{:romaji} b-c-s)
+                                              (re-seq #"[\s　]" (str c))
+                                              (#{:hiragana :katakana :kanji :symbols :commas} a-c-s)) nil
+                                         (and (#{:hiragana :katakana :kanji :symbols :commas} b-c-s)
+                                              (re-seq #"[\s　]" (str c))
+                                              (#{:romaji :commas} a-c-s)) nil
+                                         :else c)))
+                                   (drop 1 s) (drop 2 s) (drop 3 s))))
+         (subs s (- (count s) 1)))))
+
+(defn clean-text [text title journal]
+  (if (empty? text)
+    ""
+    (sequence
+     (comp (take-while #(not (or (re-seq #"^.{0,5}(参\s?[考照]|引\s?用)\s?文\s?献" %)
+                                 (re-seq #"[【（].{0,4}(参[考照]|引用)文献[】）]" %))))
+           (map (partial remove-title title journal))
+           (filter identity)
+           (remove #(re-seq #"^\s*$" %))
+           (filter (partial is-japanese? ""))
+           (map utils/convert-half-to-fullwidth))
+     (-> (apply str (filter (fn [c] (not (Character/isISOControl c))) text))
+         (string/replace #"(?m)\n[\d０-９]+\n" "")
+         (string/replace #"\n" "")
+         (string/replace #"(\s)\s+" "$1")
+         remove-space-between-chars
+         utils/split-japanese-sentence))))
+
+(defn multi-extract
+  ([path]
+   (multi-extract path "" ""))
+  ([path title journal]
+   (let [text-maps
+         [#_{:program :default :text (text/extract path)}
+          #_{:program :pdftotext-raw-text :text (sh/with-programs [pdftotext] (pdftotext "-raw" path "-"))}
+          {:program :pdftotext-raw-layout-text :text (sh/with-programs [pdftotext] (pdftotext "-raw" "-layout" path "-"))}
+          #_{:program :pdftotext-text :text (sh/with-programs [pdftotext] (pdftotext path "-"))} ;; Does not work for
+          #_{:program :pdfminer :text (try (sh/let-programs [pdf2txt "/usr/bin/pdf2txt.py"] (pdf2txt "-VA" path))
+                                           (catch clojure.lang.ExceptionInfo e ""))}
+          {:program :podofotxtextract-text :text (try (sh/with-programs [podofotxtextract] (podofotxtextract path))
+                                                      (catch clojure.lang.ExceptionInfo e ""))}]]
+     (->> text-maps
+          (map
+           (fn [m]
+             (let [cleaned-text (string/join "\n" (clean-text (:text m) title journal))]
+               (-> m
+                   (assoc :text cleaned-text)
+                   (assoc :chars (count cleaned-text))))))))))
